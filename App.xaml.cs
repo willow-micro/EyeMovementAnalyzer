e284ea7ef41828acc9b43b946f714786ac4af710
noka <account@bit-plus.work>
2021-10-20 18:35:25 +0900

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EyeMovementAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTEyNjcyQDMxMzkyZTMzMmUzMFpFdkR2V0pyOEp6aUpzdlZ5ZGVTc1RUK2JwdjdmUmFsMWZDeXNZSUhzZmM9");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿// Default
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Syncfusion.SfSkinManager;
+// Additional
+using Syncfusion.UI.Xaml.Charts;
+
+
+namespace EyeMovementAnalyzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		#region Fields4SF
+        private string currentVisualStyle;
+		private string currentSizeMode;
+        #endregion
+
+        #region Properties4SF
+        /// <summary>
+        /// Gets or sets the current visual style.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentVisualStyle
+        {
+            get
+            {
+                return currentVisualStyle;
+            }
+            set
+            {
+                currentVisualStyle = value;
+                OnVisualStyleChanged();
+            }
+        }
+		
+		/// <summary>
+        /// Gets or sets the current Size mode.
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string CurrentSizeMode
+        {
+            get
+            {
+                return currentSizeMode;
+            }
+            set
+            {
+                currentSizeMode = value;
+                OnSizeModeChanged();
+            }
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+			this.Loaded += OnLoaded;
+        }
+
+		/// <summary>
+        /// [SFDefault] Called when [loaded].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CurrentVisualStyle = "FluentLight";
+	        CurrentSizeMode = "Default";
+        }
+        /// <summary>
+        /// [SFDefault] On Visual Style Changed.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnVisualStyleChanged()
+        {
+            VisualStyles visualStyle = VisualStyles.Default;
+            Enum.TryParse(CurrentVisualStyle, out visualStyle);            
+            if (visualStyle != VisualStyles.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetVisualStyle(this, visualStyle);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
+        /// <summary>
+        /// [SFDefault] On Size Mode Changed event.
+        /// </summary>
+        /// <remarks></remarks>
+        private void OnSizeModeChanged()
+        {
+            SizeMode sizeMode = SizeMode.Default;
+            Enum.TryParse(CurrentSizeMode, out sizeMode);
+            if (sizeMode != SizeMode.Default)
+            {
+                SfSkinManager.ApplyStylesOnApplication = true;
+                SfSkinManager.SetSizeMode(this, sizeMode);
+                SfSkinManager.ApplyStylesOnApplication = false;
+            }
+        }
+
+        /// <summary>
+        /// Ask user to close or not when the Esc key was pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AppCloseEvent(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to close?", "Close", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                //this.eyeTracker.StopReceivingGazeData();
+                this.Close();
+            }
+        }
+    }
+}
